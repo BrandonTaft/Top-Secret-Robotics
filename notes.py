2a378fdec47809e72dcd36843d80416c5840c03d
Btaft6278 <btaft6278@gmail.com>
2022-01-17 14:44:46 -0500

    added notes

---
+++ notes.py
@@ -0,0 +1,8 @@
+#Command Line camera commands
+
+#take pic and put on desktop
+#*****raspistill -o Desktop/image-small.jpg -w 640 -h 480*****
+
+#record video
+#*******raspivid -o Desktop/video.h264
+
